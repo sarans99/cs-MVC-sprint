071949865790e3890db3fec2d5082e562f64aa33
tomlenton <twlenton@gmail.com>
2026-04-24 12:18:42 +0100

    Added author properties

--- cs-MVC-sprint/Models/Author.cs
+++ cs-MVC-sprint/Models/Author.cs
@@ -1,6 +1,11 @@
-﻿namespace cs_MVC_sprint.Models
+﻿using System.Globalization;
+
+namespace cs_MVC_sprint.Models
 {
     public class Author
     {
+        public int Id {get; set;}
+        public string? Name { get; set; }
+        public string? Nationality { get; set; }
     }
 }
