fc6a5041087edeeacea3b982175a3e084c390f3f
tomlenton <twlenton@gmail.com>
2026-04-24 14:31:47 +0100

    Added AuthorsController

---
+++ cs-MVC-sprint/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using cs_MVC_sprint.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using cs_MVC_sprint.Services;
+
+namespace cs_MVC_sprint.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorsController : ControllerBase
+    {
+        public IAuthorService _authorService;
+        public AuthorsController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAuthors()
+        {
+            var authors = _authorService.GetAllAuthors();
+            return Ok(authors);
+        }
+    }
+}
